71ccc1391af0c465f31faf8db58faa07bfdcfeb8
kosh04 <shigeru.kb@gmail.com>
2018-07-06 02:12:17 +0900

    Monky patching for elnode
    `:nowait` option not needed when make-network-process server mode

--- elnode-patch.el
+++ elnode-patch.el
@@ -16,4 +16,39 @@
   (when (process-live-p httpcon)
     (elnode--http-post-body httpcon)))
 
+(defun elnode/make-service (host port service-mappings request-handler defer-mode)
+  "Make an actual server TCP or Unix PORT.
+
+If PORT is a number then a TCP port is made on the specified HOST
+on the PORT.
+
+If PORT is a string a Unix socket is made in \"/tmp/\" and HOST
+is ignored."
+  (let* ((name (format "*elnode-webserver-%s:%s*" host port))
+         (an-buf (get-buffer-create name))
+         (unix-sock-file-name (unless (numberp port) (concat "/tmp/" port)))
+         (proc-args
+          (list
+           :name name
+           :buffer an-buf
+           :server (if (numberp port) 300 't)
+           ;;:nowait 't
+           :host (cond
+                   ((not (numberp port)) nil)
+                   ((equal host "localhost") 'local)
+                   ((equal host "*") nil)
+                   (t host))
+           :coding '(raw-text-unix . raw-text-unix)
+           :family (if (numberp port) 'ipv4 'local)
+           :service (if (numberp port) port unix-sock-file-name)
+           :filter 'elnode--filter
+           ;;:sentinel 'elnode--sentinel
+           :log 'elnode/proc-log))
+         (proc (apply 'make-network-process proc-args)))
+    (elnode/con-put proc
+      :elnode-service-map service-mappings
+      :elnode-http-handler request-handler
+      :elnode-defer-mode defer-mode)
+    proc))
+
 (provide 'elnode-patch)
