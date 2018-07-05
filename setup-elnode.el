6beff159ffcf70a372aa5d55214efa52c9bf58b3
kosh04 <shigeru.kb@gmail.com>
2018-07-05 18:48:18 +0900

    Temporary fix about HTTPS marmalade-repo.org issue

--- setup-elnode.el
+++ setup-elnode.el
@@ -2,18 +2,19 @@
 
 ;; load unless using Cask
 
+(require 'nsm)
 (require 'package)
 
-(add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/") t)
-(add-to-list 'package-archives
-             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/"))
 (message "package archives configured added")
 
 (package-initialize)
 (message "packages initialized")
 
-(package-refresh-contents)
+;; FIXME: avoid secutiry connection issue in marmalade-repo.org:443 (emacs-26.1 -lgnutls)
+(let ((network-security-level 'low))
+  (package-refresh-contents))
 (message "packages refreshed")
 
 (package-install 'elnode)
